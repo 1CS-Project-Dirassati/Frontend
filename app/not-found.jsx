e42c53a263a482a07b847402a852b1791cd5d4a2
Aminemazari <mazariamine095@gmail.com>
2025-04-07 18:29:45 +0100

    pulling

---
+++ app/not-found.jsx
@@ -0,0 +1,9 @@
+import Image from "../assets/error.svg";
+export default function error(){
+  return(
+    <div>
+      <h1>The page you are requesting is not available</h1>
+      <img src={"../assets/error.svg"}/>
+    </div>
+  )
+}
